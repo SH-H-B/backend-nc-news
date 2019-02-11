a19e844bddefe6f98c646a7cb7052b44f07d8fdc
SH-H-B <shiva19173@gmail.com>
2019-02-11 12:58:12 +0000

    migration(db/migrations)done! & package json && connection are modified

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,7 @@
+const ENV = process.env.NODE_ENV || "development";
 
+const dbConfig = require("../knexfile");
+
+const connection = require("knex")(dbConfig);
+
+module.exports = connection;
---
+++ db/migrations/20190211121355_create_topic_table.js
@@ -0,0 +1,17 @@
+// to run this file --> package.json---> npm run migrate:latest
+///* Create table with knex.... It is essential that the up function returns a promise so the return statement in this function is important! */
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("topics", topicTable => {
+    topicTable
+      .string("slug")
+      .primary()
+      .unique()
+      .notNullable();
+    topicTable.text("description");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("topics");
+};
