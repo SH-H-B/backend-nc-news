fd8521491af212ab878f3253e3c0ab9d14fb6f04
SH-H-B <shiva19173@gmail.com>
2019-05-14 14:20:47 +0100

    even better

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 const ENV = process.env.NODE_ENV || "development";
-const connection = require("knex")(dbConfig);
+const connection = require("knex");
 const dbConfig =
   ENV === "production"
     ? { client: "pg", connection: process.env.DATABASE_URL }
     : require("../knexfile");
 
-module.exports = connection;
+module.exports = connection(dbConfig);
