3d395d4883325d59fb04e00e2830d14d2d1bb2a5
SH-H-B <shiva19173@gmail.com>
2019-05-29 17:48:45 +0100

    updated article model

--- models/article.js
+++ models/article.js
@@ -1,24 +1,24 @@
-const connection = require('../db/connection');
+const connection = require("../db/connection");
 
 exports.getarticlesData = ({
   author,
   topic,
-  sort_by = 'created_at',
-  order = 'desc',
+  sort_by = "created_at",
+  order = "desc"
 }) => {
   const query = connection
-    .select('articles.*')
-    .from('articles')
-    .leftJoin('comments', 'articles.article_id', 'comments.article_id')
-    .count('comments.article_id as comment_count')
+    .select("articles.*")
+    .from("articles")
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .count("comments.article_id as comment_count")
     .groupBy(
-      'articles.author',
-      'articles.body',
-      'articles.title',
-      'articles.article_id',
-      'articles.votes',
-      'articles.created_at',
-      'articles.topic',
+      "articles.author",
+      "articles.body",
+      "articles.title",
+      "articles.article_id",
+      "articles.votes",
+      "articles.created_at",
+      "articles.topic"
     )
     .orderBy(sort_by, order);
   // connection.schema
@@ -35,56 +35,61 @@ exports.getarticlesData = ({
   //   .catch(err => {});
 
   if (author) {
-    query.where('articles.author', author);
+    query.where("articles.author", author);
   }
-  if (topic) {
-    query.where('articles.topic', topic);
+  if (topic !== null) {
+    query.where("articles.topic", topic);
   }
 
   return query;
 };
 
-exports.insertArticleData = newArticleData => connection
-  .insert(newArticleData)
-  .into('articles')
-  .returning('*');
+exports.insertArticleData = newArticleData =>
+  connection
+    .insert(newArticleData)
+    .into("articles")
+    .returning("*");
 
-exports.getArticleByID = ({ article_id }) => connection
-  .select('articles.*')
-  .from('articles')
-  .leftJoin('comments', 'articles.article_id', 'comments.article_id')
-  .count('comments.article_id as comment_count')
-  .where('articles.article_id', article_id)
-  .groupBy(
-    'articles.author',
-    'articles.body',
-    'articles.title',
-    'articles.article_id',
-    'articles.votes',
-    'articles.created_at',
-    'articles.topic',
-  );
+exports.getArticleByID = ({ article_id }) =>
+  connection
+    .select("articles.*")
+    .from("articles")
+    .leftJoin("comments", "articles.article_id", "comments.article_id")
+    .count("comments.article_id as comment_count")
+    .where("articles.article_id", article_id)
+    .groupBy(
+      "articles.author",
+      "articles.body",
+      "articles.title",
+      "articles.article_id",
+      "articles.votes",
+      "articles.created_at",
+      "articles.topic"
+    );
 
 exports.updateArticleVotes = ({ article_id }, { inc_votes }) => {
   if (inc_votes === undefined) inc_votes = 0;
-  const query = connection.from('articles').where('article_id', article_id);
-  query.increment('votes', inc_votes);
+  const query = connection.from("articles").where("article_id", article_id);
+  query.increment("votes", inc_votes);
 
-  return query.returning('*');
+  return query.returning("*");
 };
 
-exports.removeArticleByID = ({ article_id }) => connection
-  .from('comments')
-  .where('comments.article_id', '=', article_id)
-  .del()
-  .then(() => connection
-    .from('articles')
+exports.removeArticleByID = ({ article_id }) =>
+  connection
+    .from("comments")
+    .where("comments.article_id", "=", article_id)
     .del()
-    .where('articles.article_id', '=', article_id));
+    .then(() =>
+      connection
+        .from("articles")
+        .del()
+        .where("articles.article_id", "=", article_id)
+    );
 exports.getArticlesComments = ({
   article_id,
-  sort_by = 'created_at',
-  order = 'desc',
+  sort_by = "created_at",
+  order = "desc"
 }) => {
   const query = connection
     // .from("comments")
@@ -99,9 +104,9 @@ exports.getArticlesComments = ({
     //   "comments.body"
     // )
     // .returning("*");<--- this works but in comments table we have a column article_id so we can use comments table and no need for this
-    .from('comments')
-    .select('article_id', 'comment_id', 'votes', 'created_at', 'author', 'body')
-    .where('comments.article_id', article_id)
+    .from("comments")
+    .select("article_id", "comment_id", "votes", "created_at", "author", "body")
+    .where("comments.article_id", article_id)
     .orderBy(sort_by, order);
 
   // if (sort_by === undefined && order === undefined) {
@@ -110,8 +115,9 @@ exports.getArticlesComments = ({
   return query;
 };
 
-exports.insertCommentsByArticleID = ({ article_id }, newCommentData) => connection
-  .insert(newCommentData)
-  .where('comments.article_id', article_id)
-  .into('comments')
-  .returning('*');
+exports.insertCommentsByArticleID = ({ article_id }, newCommentData) =>
+  connection
+    .insert(newCommentData)
+    .where("comments.article_id", article_id)
+    .into("comments")
+    .returning("*");
