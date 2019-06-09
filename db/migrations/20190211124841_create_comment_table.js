51762291cdd2414cf21dbcf482167ce52c946da3
SH-H-B <shiva19173@gmail.com>
2019-06-09 23:22:10 +0100

    article model updated | Seed updated to show time

--- db/migrations/20190211124841_create_comment_table.js
+++ db/migrations/20190211124841_create_comment_table.js
@@ -1,20 +1,20 @@
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable('comments', (commentsTable) => {
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("comments", commentsTable => {
     commentsTable
-      .increments('comment_id')
+      .increments("comment_id")
       .primary()
       .notNullable();
     commentsTable
-      .string('author')
-      .references('users.username')
+      .string("author")
+      .references("users.username")
       .notNullable();
-    commentsTable.integer('article_id').references('articles.article_id');
-    commentsTable.integer('votes').defaultTo(0);
-    commentsTable.date('created_at').defaultTo(knex.fn.now());
-    commentsTable.text('body').notNullable();
+    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable.datetime("created_at").defaultTo(knex.fn.now());
+    commentsTable.text("body").notNullable();
   });
 };
 
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('comments');
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("comments");
 };
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,39 +1,37 @@
-const {
-  topicData, userData, articleData, commentData,
-} = require('../data');
-const { createArticLookup } = require('../../utiles/index');
+const { topicData, userData, articleData, commentData } = require("../data");
+const { createArticLookup } = require("../../utiles/index");
 
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
-    .then(() => knex('topics').insert(topicData))
-    .then(() => knex('users').insert(userData))
+    .then(() => knex("topics").insert(topicData))
+    .then(() => knex("users").insert(userData))
     .then(() => {
       const formatedArticlesData = articleData.map(eachArticle => ({
         title: eachArticle.title,
         body: eachArticle.body,
         topic: eachArticle.topic,
         author: eachArticle.author,
-        created_at: new Date(eachArticle.created_at),
-        votes: eachArticle.votes,
+        created_at: new Date(eachArticle.created_at).toISOString(),
+        votes: eachArticle.votes
       }));
       // console.log(formatedArticlesData);
       return knex
         .insert(formatedArticlesData)
-        .into('articles')
-        .returning('*');
+        .into("articles")
+        .returning("*");
     })
-    .then((insertedArticlesData) => {
+    .then(insertedArticlesData => {
       // console.log(insertedArticlesData[0]);
       const lookUpObject = createArticLookup(insertedArticlesData);
       const commentsWithArticlesID = commentData.map(eachComment => ({
         body: eachComment.body,
         author: eachComment.created_by,
         votes: eachComment.votes,
-        created_at: new Date(eachComment.created_at),
-        article_id: lookUpObject[eachComment.belongs_to],
+        created_at: new Date(eachComment.created_at).toISOString(),
+        article_id: lookUpObject[eachComment.belongs_to]
       }));
-      return knex.insert(commentsWithArticlesID).into('comments');
+      return knex.insert(commentsWithArticlesID).into("comments");
     });
 };
